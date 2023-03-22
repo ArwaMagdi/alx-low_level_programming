471e8c719ddc92aa802e80e52c633714da84decb
ArwaMagdi <arwamagdi20@gmail.com>
2023-03-22 16:31:21 -0700

    m

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - printall alphabet in lowercase 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	char low;
+	for (int i=0;i<10;i++)
+	  {
+	    for (low = 'a'; low <= 'z'; low++)
+	      putchar(low);
+	  }
+	putchar('\n');
+}
