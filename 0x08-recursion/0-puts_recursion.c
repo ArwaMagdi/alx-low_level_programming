d4e57968c7bec3cbcad51f931e677abf9738be66
Arwa Magdi <arwamagdi20@gmail.com>
2023-04-29 03:35:55 +0300

    m

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,13 +9,23 @@ void _puts_recursion(char *s)
 {
 	if(*s)
 	{
-		write(1, &s, 1);
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 	else
 	{
-		write(1, &'\n', 1);
+		_putchar('\n');
 	}
 }
-
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+  return (write(1, &c, 1));
+}
 
