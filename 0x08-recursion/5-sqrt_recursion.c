25ad081cf156723292e2ed244fe90da10dfb0129
Arwa Magdi <arwamagdi20@gmail.com>
2023-04-29 05:35:06 +0300

    m

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,18 +12,3 @@ int _sqrt_recursion(int n)
 
 	return (_sqrt(1, n));
 }
-
-/**
- * _sqrt - find square root
- * @prev: previous value
- * @root: square root value
- * Return: the square root
- */
-int _sqrt(int prev, int root)
-{
-	if (prev > root)
-		return (-1);
-	else if (prev * prev == root)
-		return (prev);
-	return (_sqrt(prev + 1, root));
-}
