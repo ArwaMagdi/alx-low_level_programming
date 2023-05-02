19ca6e445bde0c48ba218bceb09e62507f33468f
Arwa Magdi <arwamagdi20@gmail.com>
2023-05-02 16:25:07 +0300

    m

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#define dog_h
+
+/**
+ * struct dog - structure for a basic dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Description: data structure for dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
