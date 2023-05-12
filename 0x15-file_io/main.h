d92bda2c5c4234f1b3b268533d2427102b43b5dc
Arwa Magdi <arwamagdi20@gmail.com>
2023-05-13 02:28:41 +0300

    m

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,5 +9,6 @@
 #include <stdlib.h>
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
